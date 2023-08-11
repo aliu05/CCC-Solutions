4a92ef178c8cb155bbce5623dbc26c7780366085
Andrew Liu <105452729+aliu05@users.noreply.github.com>
2023-08-11 18:56:23 +0800

    Update J4.cpp

--- 2023/Junior/J4.cpp
+++ 2023/Junior/J4.cpp
@@ -4,35 +4,35 @@ using namespace std;
 #define ll long long
 #define nl "\n"
 
-const int mxN=2e5;
+const int mxN = 2e5;
 int N, inp[2][mxN], ans;
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> N;
-    for(int i=0; i<2; ++i) {
-        for(int j=0; j<N; ++j) {
+    for(int i = 0; i < 2; ++i) {
+        for(int j = 0; j < N; ++j) {
             cin >> inp[i][j];
             if(inp[i][j])
-                ans+=3;
+                ans += 3;
         }   
     }
     
-    for(int i=0; i<N-1; ++i) {
+    for(int i = 0; i < N - 1; ++i) {
         if(inp[0][i]) {
             if(inp[0][i+1])
-                ans-=2;
-            if(inp[1][i] && i%2==0)
-                ans-=2;
+                ans -= 2;
+            if(inp[1][i] && i % 2 == 0)
+                ans -= 2;
         }
         if(inp[1][i] && inp[1][i+1]) {
-            ans-=2;
+            ans -= 2;
         }
     }
     
-    if(N%2==1 && inp[0][N-1] && inp[1][N-1])
-        ans-=2;
+    if(N % 2 == 1 && inp[0][N-1] && inp[1][N-1])
+        ans -= 2;
     cout << ans;
     return 0;
 }
